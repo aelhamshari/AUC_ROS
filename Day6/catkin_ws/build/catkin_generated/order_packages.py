3d8bec0860aaf2c61178d476b6cd87d2b28c2c2a
Afnan <afnan.khalid1997@gmail.com>
2020-08-29 14:59:33 +0200

    modified launch files

--- Day6/catkin_ws/build/catkin_generated/order_packages.py
+++ Day6/catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/afnan/Desktop/AUC_ROS/Day6/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/afnan/Desktop/AUC_ROS/Day6/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/afnan/Desktop/AUC_ROS/Day6/catkin_ws/devel;/opt/ros/melodic' != '' else []
