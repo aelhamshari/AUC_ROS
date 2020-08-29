490cf37252391e65c370bacb826e0e440966426a
Afnan <afnan.khalid1997@gmail.com>
2020-08-29 10:46:25 +0200

    Package build and devel files

---
+++ Day6/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/afnan/Desktop/AUC_ROS/Day6/catkin_ws/src
+# Build directory: /home/afnan/Desktop/AUC_ROS/Day6/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_first_robot")
